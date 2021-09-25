671ee5fedcf00f65020f6432177be086bb64a240
Kirillova Olesya <olesyak-85@mail.ru>
2021-09-25 21:31:22 +0300

    added maven and failNoTest, classes Product, Book, Smartphone.

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,21 @@
+package ru.netology.domain;
+
+public class Book extends Product {
+    private String author;
+
+    public Book(int id, String name, double price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+    public Book() {
+        super();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
